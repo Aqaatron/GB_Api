cc0aaa62c6dd9f2136328a5acfab00527ededc4a
Андрей Cидоров <aqaatron@gmail.com>
2021-05-17 11:00:22 +0300

    prac3

---
+++ MetricsService/MetricsService/MetricsAgentTests/DotNetMetricsControllerUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xunit;
+using MetricsAgent;
+using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using MetricsManager.Enums;
+using Moq;
+using MetricsAgent.Repositories;
+using MetricsAgent.MetricsClasses;
+using Microsoft.Extensions.Logging;
+
+namespace MetricsAgentTests
+{
+    public class DotNetMetricsControllerUnitTests
+    {
+        private DotNetMetricsController controller;
+
+        private Mock<ILogger<DotNetMetricsController>> mockLogger;
+
+        private Mock<IDotNetMetricsRepository> mockRepository;
+
+        public DotNetMetricsControllerUnitTests()
+        {
+            this.mockLogger = new Mock<ILogger<DotNetMetricsController>>();
+
+            this.mockRepository = new Mock<IDotNetMetricsRepository>();
+
+            this.controller = new DotNetMetricsController(mockLogger.Object, mockRepository.Object);
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            //Act
+            var result = controller.GetMetrics(fromTime, toTime);
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+
+        [Fact]
+        public void Create_ShouldCall_Create_From_Repository()
+        {
+            // устанавливаем параметр заглушки
+            // в заглушке прописываем что в репозиторий прилетит CpuMetric объект
+            mockRepository.Setup(repository => repository.Create(It.IsAny<DotNetMetric>())).Verifiable();
+
+            // выполняем действие на контроллере
+            //var result = controller.Create(new MetricsAgent.Requests.CpuMetricCreateRequest { Time = TimeSpan.FromSeconds(1), Value = 50 });
+
+            // проверяем заглушку на то, что пока работал контроллер
+            // действительно вызвался метод Create репозитория с нужным типом объекта в параметре
+            mockRepository.Verify(repository => repository.Create(It.IsAny<DotNetMetric>()), Times.AtMostOnce());
+        }
+    }
+}
---
+++ MetricsService/MetricsService/MetricsAgentTests/RamMetricsControllerUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xunit;
+using MetricsAgent;
+using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using MetricsManager.Enums;
+using Moq;
+using MetricsAgent.Repositories;
+using MetricsAgent.MetricsClasses;
+using Microsoft.Extensions.Logging;
+
+namespace MetricsAgentTests
+{
+    public class RamMetricsControllerUnitTests
+    {
+        private RamMetricsController controller;
+
+        private Mock<ILogger<RamMetricsController>> mockLogger;
+
+        private Mock<IRamMetricsRepository> mockRepository;
+
+        public RamMetricsControllerUnitTests()
+        {
+            this.mockLogger = new Mock<ILogger<RamMetricsController>>();
+
+            this.mockRepository = new Mock<IRamMetricsRepository>();
+
+            this.controller = new RamMetricsController(mockLogger.Object, mockRepository.Object);
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            //Act
+            var result = controller.GetMetrics(fromTime, toTime);
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void Create_ShouldCall_Create_From_Repository()
+        {
+            // устанавливаем параметр заглушки
+            // в заглушке прописываем что в репозиторий прилетит CpuMetric объект
+            mockRepository.Setup(repository => repository.Create(It.IsAny<RamMetric>())).Verifiable();
+
+            // выполняем действие на контроллере
+            //var result = controller.Create(new MetricsAgent.Requests.CpuMetricCreateRequest { Time = TimeSpan.FromSeconds(1), Value = 50 });
+
+            // проверяем заглушку на то, что пока работал контроллер
+            // действительно вызвался метод Create репозитория с нужным типом объекта в параметре
+            mockRepository.Verify(repository => repository.Create(It.IsAny<RamMetric>()), Times.AtMostOnce());
+        }
+    }
+}
